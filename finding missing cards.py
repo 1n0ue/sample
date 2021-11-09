ea1d635b3553a0081e641a9cb32ec7a4bbba7329
1n0ue <s246023@wakayama-u.ac.jp>
2021-11-09 17:29:10 +0900

    2021/11/09 firstcommit

---
+++ finding missing cards.py
@@ -0,0 +1,11 @@
+all_cards = [(s,n) for s in ["S","H","D","C"] for n in range(1,14)]
+#for_inで配列代入
+n = int(input())
+hold_cards = [] #空のリスト
+for i in range(n):
+    suit,num = input().split()
+    num = int(num) #キャスト
+    hold_cards.append((suit,num)) #タプル
+for cards in all_cards:
+    if cards not in set(hold_cards): #not_inで含まれない場合実行
+        print(*cards)
